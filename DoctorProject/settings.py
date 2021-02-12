00dd8f4b0e83f96e8ccc3c320b7b5ca5ec8ba4cd
Tim <l4kk3r@yandex.ru>
2021-02-12 23:43:08 +0300

    whitenoise

--- DoctorProject/settings.py
+++ DoctorProject/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = ['easydoctor-test.herokuapp.com', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
+    'django.middleware.security.SecurityMiddleware',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -116,5 +117,5 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
