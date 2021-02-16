d74cae5749f6b055caafae9e31aa369116a9342e
l4kk3r <66743739+l4kk3r@users.noreply.github.com>
2021-02-16 14:05:53 +0300

    Update models.py

--- main_app/models.py
+++ main_app/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Record(models.Model):
-    doctor_ops = [('Никифорова Елизавета Юрьевна', 'Никифорова Елизавета Юрьевна'), ('Анотьева Ирина Михайловна', 'Анотьева Ирина Михайловна') ]
+    doctor_ops = [('Анатольева Елизавета Юрьевна', 'Анатольева Елизавета Юрьевна'), ('Волкова Ирина Михайловна', 'Волкова Ирина Михайловна') ]
     time_ops = [("18:00", "18:00"), ("19:00", "19:00"), ("20:00","20:00"),("21:00","21:00"),("22:00","22:00")]
     patient = models.ForeignKey(User, on_delete=models.DO_NOTHING)
     doctor = models.CharField(max_length=200, choices=doctor_ops)
